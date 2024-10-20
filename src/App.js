e0cf748a566989344ecabb76327eff1efa726359
Ex0TiiC24 <taiyophamadadar6@gmail.com>
2024-10-21 00:29:42 +0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,14 +7,22 @@ import { useState,useEffect } from "react";
 function App() {
   const [display,settime] = useState('0:00:00 PM')
   const [dark,setDark] = useState(false)
+  const [mode,setMode] = useState('Darkmode')
   
   const darkModeHandler = () => {
+    if(mode==="Darkmode"){
+      setMode("Lightmode")
+    }
+    else{
+      setMode("Darkmode")
+    }
     setDark(!dark);
     document.body.classList.toggle("dark");
+    
+
 }
   useEffect(()=>{
     setInterval(()=>{
-      
         const currentDate = new Date();
         settime(currentDate.toLocaleTimeString())
         
@@ -27,8 +35,8 @@ function App() {
     <div class="flex justify-center items-center w-full h-screen flex-col dark:bg-zinc-800 dark:text-white">
       <div class="text-3xl">{display}</div>
       <div>
-        <button class="border-solid border-2 mt-3 w-10"
-        onClick={darkModeHandler}>hi</button>
+        <button class="border-solid border-2 mt-3 w-fit px-1"
+        onClick={darkModeHandler}>{mode}</button>
       </div>
     </div>
   );
